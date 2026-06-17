fdc4f78fd6b4d0297f5535efad8d2faaea9ce259
noahc-aa <noahc@automatedanalytics.co.uk>
2026-06-17 16:10:05 +0100

    attempt for top strip 2.0

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div class="top-strip">
+    <div class="container px-6 py-4 text-center">
+      <h1 class="text-2xl font-bold">Soteria</h1>
+    </div>
+  </div>
   <div class="container px-6 py-40 text-left">
     <h1 class="text-6xl font-bold">Praeventio</h1>
   </div>
 `
-<div class="container px-0 py-100 text-center">
-  <h1 class="text-6xl font-bold">Soteria</h1>
-  background-color: #0a0078;
-</div>

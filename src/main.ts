3490a0561eb747d2096e748cfb7a07b7b668fa51
noahc-aa <noahc@automatedanalytics.co.uk>
2026-06-17 15:59:22 +0100

    new title and some formatting

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div class="container px-6 py-20 text-center">
-    <h1 class="text-6xl font-bold">Brochure Test</h1>
-    <p class="mt-6">This is a test paragraph with a blue gradient background.</p>
+  <div class="container px-6 py-40 text-left">
+    <h1 class="text-6xl font-bold">Praeventio</h1>
   </div>
 `

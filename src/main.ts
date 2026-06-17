f8540a57fc228e9718ad92869440de7e04394c77
noahc-aa <noahc@automatedanalytics.co.uk>
2026-06-17 14:20:08 +0100

    testing tailwind again

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <h1 class="text-3xl font-bold text-center">Brochure Test</h1>
+  <h1 class="text-6xl font-bold text-center">Brochure Test</h1>
 `

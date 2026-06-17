7d1c02c463e77b5074fa2298dea11a9bd96a693c
noahc-aa <noahc@automatedanalytics.co.uk>
2026-06-17 16:27:18 +0100

    Fixed top strip and added in the navigation bar (links not yet working)

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import './style.css'
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="top-strip">
     <div class="container px-6 py-4 text-center">
-      <h1 class="text-2xl font-bold">Soteria</h1>
+      <h1 class="text-2xl font-bold">Soteria Inc</h1>
     </div>
   </div>
   <div class="container px-6 py-40 text-left">

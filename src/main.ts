d890bc08d2fcba2d11b1f79acb532843869f3dba
noahc-aa <noahc@automatedanalytics.co.uk>
2026-06-17 15:30:04 +0100

    testing background colour change

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,8 @@
 import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <h1 class="text-6xl font-bold text-center">Brochure Test</h1>
-
-  <style>
-    :root {
-      --colour-blue-1: #d6e4f5;
-      --colour-blue-2: #a4c7df;
-      --colour-blue-3: #7bb7d1;
-      --colour-blue-4: #4aa0c4;
-      --colour-blue-5: #1d3a6d;
-    }
-  </style>
-
-  <div style="background-color: radial-gradient(circle, var(--colour-blue-1), var(--colour-blue-2) var(--colour-blue-3) var(--colour-blue-4) var(--colour-blue-5)); color: white; padding: 20px; text-align: center;">
-    <p>This is a test paragraph with a blue gradient background.</p>
+  <div class="container px-6 py-20 text-center">
+    <h1 class="text-6xl font-bold">Brochure Test</h1>
+    <p class="mt-6">This is a test paragraph with a blue gradient background.</p>
   </div>
 `

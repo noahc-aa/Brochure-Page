ba2c65dde5d202f6cf3e91025f16b1de65ffecb5
noahc-aa <noahc@automatedanalytics.co.uk>
2026-06-17 15:17:05 +0100

    testing colour changes

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,12 @@ import './style.css'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <h1 class="text-6xl font-bold text-center">Brochure Test</h1>
+
+  @theme {
+  --colour-mint-500: #3EB489;
+  --colour-mint-600: #2E8B57;
+  --colour-mint-700: #006400;
+  --colour-mint-800: #004d40;
+  --colour-mint-900: #00332c;
+  }
 `

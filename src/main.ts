327416bb5f79294db53a75b95dd2786192940887
noahc-aa <noahc@automatedanalytics.co.uk>
2026-06-18 09:40:47 +0100

    adjusted logo

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
   <div class="top-strip">
     <div class="container top-strip-content">
       <div class="brand">
-        <img src="/praeventio_shield_v3.png" alt="Praesidium logo" class="brand-logo" />
-        <h1 class="text-3xl font-bold">Praesidium</h1>
+        <img src="/praeventio_shield_v3.png" alt="Salus Inc logo" class="brand-logo" />
+        <h1 class="text-3xl font-bold">Salus Inc</h1>
       </div>
       <nav class="top-strip-nav">
         <span class="text-lg font-semibold underline">About Us</span>
